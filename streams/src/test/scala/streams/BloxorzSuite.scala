f4276114d4b8ffa0042bee26a5209decea1ad205
Wooangha <85687189+Wooangha@users.noreply.github.com>
2025-10-13 01:05:43 +0900

    test: default test cases for neighborsWithHistory, and newNeighborsOnly

--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -53,6 +53,35 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("neighborsWithHistory default test") {
+    new Level1 {
+      assert(
+        neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up)).toSet
+        ===
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        )
+      )
+    }
+  }
+
+  test("newNeighborsOnly default test") {
+    new Level1 {
+      val ret = newNeighborsOnly(
+        Set(
+          (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+          (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+        ).toStream,
+        Set(Block(Pos(1,2),Pos(1,3)), Block(Pos(1,1),Pos(1,1)))
+      )
+      val ans = Set(
+                  (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+                ).toStream
+      assert(ret === ans)
+    }
+  }
+
   test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
